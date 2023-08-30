280a4fd95805caa7f4459a36563fa70eaf302fbd
yujin <121207285+8566uyu@users.noreply.github.com>
2023-08-30 20:49:41 +0900

    cors update app.js
    cors 추가

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 const todoRouter = require('./routes/todo');
 app.use('/api', todoRouter); // 기본주소: localhost:PORT/api
